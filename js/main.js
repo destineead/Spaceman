cc86b87848b6df4faa2a92ce85130a52596886e4
destineead <destineenugent1@gmail.com>
2023-12-18 16:43:21 -0500

    Stub up initialize and render functions

--- js/main.js
+++ js/main.js
@@ -4,8 +4,12 @@
 /*----- state variables -----*/
 let secret_word;
 let max_attempts;
-let guessed_word;
-let incorrect_guesses;
+let correct_guess;//count correct guesses
+let incorrect_guess;// count incorrect guesses
+let alphabet = ['a', 'b', 'c', 'd','e','f','g','h',
+                'i','j','k','l','m','n','o','p','q'
+                ,'r','s','t','u','v','w','x','y','z']
+let spaces; // number of spaces in the word
 
 /*----- cached elements  -----*/
 
@@ -14,3 +18,25 @@ let incorrect_guesses;
 
 
 /*----- functions -----*/
+init();
+
+function init() {
+    secret_word = {
+
+    };
+    max_attempts = 7;
+    correct_guess = {
+
+    };
+    incorrect_guess = {
+
+    };
+    alphabet = {
+
+    };
+    spaces = {
+
+        ``
+    }
+render();
+}
